699b63b5c6bd5567945b490066c3d23e58ee8f2d
gabrielbaltar <201902046293@alunos.estacio.br>
2025-01-21 03:09:40 -0300

    style: add banner

--- components/Banner/Banner.jsx
+++ components/Banner/Banner.jsx
@@ -1,4 +1,7 @@
 import './Banner.css';
+import bannerIcon from '../../src/assets/banner-icon.svg';
+import bannerIllustration from '../../src/assets/background-banner.png';
+import Vector from '../../src/assets/illustration.svg';
 
 const Banner = () => {
 
@@ -10,20 +13,28 @@ const Banner = () => {
                         <span></span>
                     </div>
                     <div className='left-square-secondary'>
-
+                        <img src={bannerIcon} alt="Ícone de raio" />
                     </div>
                 </div>
                 <div className='main-content'>
                     <div className='main-title'>
-                        <span></span>
+                        <h1>Designs que fascinam, tecnologias digitais que transformam.</h1>
+                        <p>Transformamos ideias em realidade digital através de designs inspiradores e interfaces intuitivas que conectam pessoas e impulsionam negócios.</p>
+                        <a href="#">Iniciar um projeto</a>
+
+                        <div className='mobile-background'>
+                            <img src={bannerIllustration} alt="Background em gradiente" />
+                        </div>
                     </div>
                     <div className='illustration'>
-
+                        <div className='vector'>
+                            <img src={Vector} alt="Ilustração do banner" />
+                        </div>
+                        <img src={bannerIllustration} alt="Background em gradiente" />
                     </div>
                 </div>
                 <div className='right-square'>
                     <div className='right-square-main'>
-                        <span></span>
                     </div>
                 </div>
             </div>
