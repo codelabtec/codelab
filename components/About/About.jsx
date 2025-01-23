f7faa9f8c97fbe9fb79b46a4ddce596995f49454
gabrielbaltar <201902046293@alunos.estacio.br>
2025-01-23 03:14:51 -0300

    style: text video

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -12,7 +12,10 @@ const About = () => {
                     <p>Somos uma agência que transforma ideias em soluções digitais únicas, unindo design criativo, tecnologia de ponta e estratégia para impulsionar marcas e alcançar resultados extraordinários.</p>
                 </div>
                 <div className='institutional-video'>
-                    <span>Vídeo de </span>
+                    <span>Vídeo de Apresentacao</span>
+                    <div className='video'>
+                        <iframe src="https://www.youtube.com/embed/5qap5aO4i9A" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                    </div>
                 </div>
             </div>
         </section>
