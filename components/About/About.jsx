8439f2a30fa04cafa3a59c74fc48366d3b5592e1
gabrielbaltar <201902046293@alunos.estacio.br>
2025-01-23 02:56:56 -0300

    style: content

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -8,6 +8,8 @@ const About = () => {
             <div className='company-content'>
                 <div className='company-text'>
                     <span>Sobre a CodeLab</span>
+                    <h2>Estratégia, design e tecnologia para negócios vencedores.</h2>
+                    <p>Somos uma agência que transforma ideias em soluções digitais únicas, unindo design criativo, tecnologia de ponta e estratégia para impulsionar marcas e alcançar resultados extraordinários.</p>
                 </div>
                 <div className='institutional-video'>
                     <span>Vídeo de Apresentação</span>
