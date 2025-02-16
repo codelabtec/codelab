ebc93cc55d7b39a5b7cbc5eaed90cbba6250fe14
gabrielbaltar <201902046293@alunos.estacio.br>
2025-02-15 23:48:19 -0300

    feat: brevo integration

--- components/Newsletter/Newsletter.jsx
+++ components/Newsletter/Newsletter.jsx
@@ -1,9 +1,34 @@
 import './Newsletter.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 const Newsletter = () => {
+    const [email, setEmail] = useState('');
+    const [status, setStatus] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post('http://localhost:5001/api/subscribe',
+            { email },
+            {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+            );
+            setStatus(response.data.message);
+            setEmail('');
+        } catch (error) {
+            const errorMessage = error.response?.data?.message || 'Erro ao realizar inscrição!';
+            const brevoMessage = error.response?.data?.error?.message;
+            setStatus(`Erro: ${errorMessage || brevoMessage || 'Falha na conexão'}`);
+        }
+    };
 
     return (
 
+        <form onSubmit={handleSubmit}>
         <section className='newsletter-container'>
             <div className='newsletter-content'>
                 <div className='newsletter-text'>
@@ -11,12 +36,21 @@ const Newsletter = () => {
                     <p>Esteja por dentro novos artigos, anúncios, e estudos de caso.</p>
                 </div>
                 <div className='newsletter-form'>
-                    <input type='email' placeholder='Digite seu e-mail' />
-                    <button>Inscreva-se</button>
+                    <input 
+                    type='email' 
+                    value={email} 
+                    placeholder='Digite seu e-mail' 
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                    />
+                    
+                    <button type='submit'>Inscreva-se</button>
+                    {status && <p>{status}</p>}
                 </div>
             </div>
 
         </section>
+        </form>
     );
 
 };
