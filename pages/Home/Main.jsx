e1e64b29607a123db55664c4572e16cd32b7175a
gabrielbaltar <201902046293@alunos.estacio.br>
2025-03-20 02:49:08 -0300

    add new seo

--- pages/Home/Main.jsx
+++ pages/Home/Main.jsx
@@ -22,10 +22,10 @@ function Main() {
     <meta property="og:title" content="CodeLab Tecnologia - Soluções Digitais que Transformam Negócios" />
     <meta property="og:description" content="Transformamos ideias em soluções digitais únicas com design criativo e tecnologia de ponta para impulsionar sua marca no mercado digital." />
     <meta property="og:type" content="website" />
-    <meta property="og:url" content="https://seusite.com/" />
-    <meta property="og:image" content="https://seusite.com/imagens/og-image.jpg" />
+    <meta property="og:url" content="https://www.codelabtec.com/" />
+    <meta property="og:image" content="https://www.codelab.com/public/thumb.png" />
     <meta name="twitter:card" content="summary_large_image" />
-    <link rel="canonical" href="https://seusite.com/" />
+    <link rel="canonical" href="https://www.codelabtec.com/" />
   </Helmet>
 
   return (
